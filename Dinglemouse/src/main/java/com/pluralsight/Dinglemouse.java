66316c00171c056e8b2fb16b7a28dc5939bd2649
Furkan <furkanamerikaa@gmail.com>
2024-11-05 08:54:19 -0500

    Codewars even number question

---
+++ Dinglemouse/src/main/java/com/pluralsight/Dinglemouse.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Dinglemouse {
+
+    public static class HelpSaveChristmas implements Comparator<String> {
+
+        @Override
+        public int compare(final String line1, final String line2) {
+            // Your comparison code goes here!
+            return 0;
+        }
+    }
+
+}
---
+++ Number/src/main/java/com/pluralsight/Number.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Number {
+    public boolean isEven (double n) {
+        if (n%1 == 0){ return (n%2 == 0);
+
+        }return false;
+    }
+}
